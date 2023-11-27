b76cf4edf72718290980692582ed308cd6800355
shtanriverdi <shtanriverdi@yahoo.com>
2023-11-27 22:00:35 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex justify-center items-center h-screen bg-[#d1d9e6]">
+      <div className="flex flex-col bg-[#ffffff] h-[36rem] w-96 rounded-[20px]">
+        <div className="basis-2/3 m-4">
+          <img className="rounded-[10px] object-contain w-full" src="./qr.png" alt="QR Image" />
+        </div>
+        <div className="basis-1/3 flex justify-start flex-col text-center">
+          <h1 className="text-[25px] font-bold mx-8 mb-2 text-[#1c2a4e]">Improve your front-end skills by building projects</h1>
+          <p className="text-[15px] mx-16 text-[#7892a1]">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ style-guide.md
@@ -0,0 +1,26 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+White: hsl(0, 0%, 100%) converts to hex #ffffff.
+Light gray: hsl(212, 45%, 89%) converts to hex #d1d9e6.
+Grayish blue: hsl(220, 15%, 55%) converts to hex #7892a1.
+Dark blue: hsl(218, 44%, 22%) converts to hex #1c2a4e.
+
+## Typography
+
+### Body Copy
+
+- Font size (paragraph): 15px
+
+### Font
+
+- Family: [Outfit](https://fonts.google.com/specimen/Outfit)
+- Weights: 400, 700
